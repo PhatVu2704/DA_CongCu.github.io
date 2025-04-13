91f3b9dc1786a459ad6823559544df6e723f02c9
VuHoangPhat <phatvu27042003@gmail.com>
2025-04-13 21:13:24 +0700

    fix router

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/DA_CongCu.github.io/', // 👈 dòng này rất quan trọng
+  // base: '/DA_CongCu.github.io/', // 👈 dòng này rất quan trọng
   server: {
     host: true,
     port: 5173,
